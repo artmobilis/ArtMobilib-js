6b479bb2cc5e5db63f6657e79e03b7353d126bbd
Christophe Vestri <vestri@gmail.com>
2015-12-18 16:31:44 +0100

    some cleaning, still ot used

--- ArtMobilib/CornerDetector.js
+++ ArtMobilib/CornerDetector.js
@@ -3,35 +3,37 @@
 // Corners detection
 // detect corners and compute ORB descriptors
 
+// todo: always sort corner 
+
 var CornerDetector = function () {
 
     /// private data
     var that = this;
-    var smoothed_img;
+    var _smoothed_img;
 
     /// public data
     this.max_per_level = 100;
-    this.blur_size;
-    this.lap_thresh;
-    this.eigen_thres;
+    this.blur_size = 5;        // 3 to 9
+    this.lap_thres = 30;       // 1 to 100
+    this.eigen_thres = 25;     // 1 to 100
 
     /// private methods
-    function detect_keypoints(img, corners, max_allowed) {
+    function DetectKeypoints(img, corners) {
         // detect features
-        jsfeat.yape06.laplacian_threshold = that.lap_thres | 0;
-        jsfeat.yape06.min_eigen_value_threshold = that.eigen_thres | 0;
+        jsfeat.yape06.laplacian_threshold = that.lap_thres;
+        jsfeat.yape06.min_eigen_value_threshold = that.eigen_thres;
 
         var count = jsfeat.yape06.detect(img, corners, 17);
 
         // sort by score and reduce the count if needed
-        if (count > max_allowed) {
+        if (count > that.max_per_level) {
             jsfeat.math.qsort(corners, 0, count - 1, function (a, b) { return (b.score < a.score); });
-            count = max_allowed;
+            count = that.max_per_level;
         }
 
         // calculate dominant orientation for each keypoint
         for (var i = 0; i < count; ++i) {
-            corners[i].angle = ic_angle(img, corners[i].x, corners[i].y);
+            corners[i].angle = IC_Angle(img, corners[i].x, corners[i].y);
         }
 
         return count;
@@ -39,7 +41,7 @@ var CornerDetector = function () {
 
     // central difference using image moments to find dominant orientation
     var u_max = new Int32Array([15, 15, 15, 15, 14, 14, 14, 13, 13, 12, 11, 10, 9, 8, 6, 3, 0]);
-    function ic_angle(img, px, py) {
+    function IC_Angle(img, px, py) {
         var half_k = 15; // half patch size
         var m_01 = 0, m_10 = 0;
         var src = img.data, step = img.cols;
@@ -67,29 +69,23 @@ var CornerDetector = function () {
         return Math.atan2(m_01, m_10);
     }
     
-    init_image = function(img) {
-        if (smoothed_img || smoothed_img.cols != img.cols || smoothed_img.rows != img.rows)
-            smoothed_img = new jsfeat.matrix_t(img.cols, img.rows, jsfeat.U8_t | jsfeat.C1_t);
-    }
-
-    /// public methods
-    this.init = function (options) {
-        that.blur_size = options.blur_size;
-        that.lap_thresh = options.blur_size;
-        that.eigen_thres = options.blur_size;
-
+    // check size compatbility
+    InitImage = function(img) {
+        if (_smoothed_img || _smoothed_img.cols != img.cols || _smoothed_img.rows != img.rows)
+            _smoothed_img = new jsfeat.matrix_t(img.cols, img.rows, jsfeat.U8_t | jsfeat.C1_t);
     }
 
     // train a pattern: extract corners multiscale, compute descriptor, store result
-    // input is greyscale image
-    this.detectCorners = function (img, corners_num, corners, descriptors, options) {
-        init_image(img);
+    // input should be greyscale image
+    this.DetectCorners = function (img, corners, descriptors) {
+        InitImage(img);
 
-        jsfeat.imgproc.gaussian_blur(img, smoothed_img, options.blur_size | 0); // this is more robust
-        that.corners_num = detect_keypoints(smoothed_img, corners, max_per_level);
-        jsfeat.orb.describe(smoothed_img, corners, corners_num, descriptors);
+        jsfeat.imgproc.gaussian_blur(img, _smoothed_img, that.blur_size); // this is more robust
+        var corners_num = detect_keypoints(_smoothed_img, corners, that.max_per_level);
+        jsfeat.orb.describe(_smoothed_img, corners, corners_num, descriptors);
 
-        console.log("IMtrain " + smoothed_img.cols + "x" + smoothed_img.rows + " points: " + that.corners_num);
+        console.log("IMtrain " + _smoothed_img.cols + "x" + _smoothed_img.rows + " points: " + corners_num);
 
+        return corners_num;
     };
 };
