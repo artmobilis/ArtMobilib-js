dd03e14d7d59c258d162f6c12088909a103617e3
Christophe Vestri <vestri@gmail.com>
2016-03-31 16:56:15 +0200

    Image debugger which display detected corners

---
+++ src/ImageDebugger.js
@@ -0,0 +1,59 @@
+var AM = AM || {};
+
+
+AM.ImageDebugger = function() {
+
+  var _context2d;
+  var _trained_image;
+  var _corners;
+  var _screen_corners;
+  var _matches;
+  var _profiler;
+  var _uuid;
+
+  var _debugMatches;
+
+
+  this.DrawCorners = function(marker_corners) {
+    if(!_debugMatches) return;
+    if(!marker_corners) return;
+
+    _screen_corners = marker_corners.screen_corners;
+    _profiler       = marker_corners.profiler;
+
+    if(!_screen_corners.length) return;
+
+    _context2d.fillStyle="red";
+  
+    for(var i = 0; i < _screen_corners.length; ++i) {
+      var sc = _screen_corners[i];
+
+      _context2d.beginPath();
+      _context2d.arc(sc.x, sc.y, 5, 0, 2 * Math.PI);
+     _context2d.fill();
+    }
+
+    // console
+  };
+
+  this.DrawMatches = function(marker_corners, trainedImage) {
+    if(!_debugMatches) return;
+    if(!marker_corners) return;
+    if(!trainedImage) return;
+
+    _corners       = marker_corners.corners;       
+    _matches       = marker_corners.matches;
+    _trained_image = trainedImage;
+
+    // draw images, its corresponding corners, and matches
+
+
+  };
+
+  this.SetData = function ( context2d, debugMatches) {
+    _context2d=context2d;
+    _debugMatches=debugMatches || false;
+  };
+
+
+};
