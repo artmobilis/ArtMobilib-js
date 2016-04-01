fd9e02a9000355ca9652c8c6b892f5378a0ea552
Christophe Vestri <vestri@gmail.com>
2016-04-01 17:41:26 +0200

    Display trained image in Imagge Debugger

--- build/artmobilib.js
+++ build/artmobilib.js
@@ -201,12 +201,18 @@ var AM = AM || {};
 AM.ImageDebugger = function() {
 
   var _context2d;
-  var _trained_image;
+  var _trained_image_url;
   var _corners;
   var _screen_corners;
   var _matches;
   var _profiler;
   var _uuid;
+  var _offsety=40;
+
+  var _last_uuid;
+  var _last_image_data;
+
+  var _image_loader = new AM.ImageLoader();
 
   var _debugMatches=false;
 
@@ -233,19 +239,33 @@ AM.ImageDebugger = function() {
     // console
   };
 
-  this.DrawMatches = function(marker_corners, trainedImage) {
+  this.DrawMatches = function(marker_corners, trained_image_url) {
     if(!_debugMatches) return;
     if(!marker_corners) return;
-    if(!trainedImage) return;
+    if(!trained_image_url) return;
 
-    _corners       = marker_corners.corners;       
-    _matches       = marker_corners.matches;
-    _trained_image = trainedImage;
+    _corners          = marker_corners.corners;       
+    _matches          = marker_corners.matches;
+    _trained_image_url = trained_image_url;
 
     // draw images, its corresponding corners, and matches
+    if(marker_corners.uuid != _last_uuid) {
+      _last_uuid=marker_corners.uuid;
 
+      _image_loader.GetImageData(trained_image_url, function(image_data) {
+            _last_image_data=image_data;
+            drawImage();
+          }, false);
+    }
 
+    if(_last_image_data)
+      drawImage();
   };
+
+  drawImage = function (){
+    console.log("image size=" + _last_image_data.width + " " + _last_image_data.height);
+    _context2d.putImageData(_last_image_data, 0, _offsety);
+  }
 
   this.SetData = function ( context2d, debugMatches) {
     _context2d=context2d;
@@ -4519,7 +4539,7 @@ AM.MarkerTracker = function() {
    * @inner
    * @returns {AM.match_t[]}
    */
-  this.Getmatches = function () {
+  this.GetMatches = function () {
     return _matching.GetMatches();
   };
 
