38a2e42cbef91e98695dfdf16640a55288bf339d
calemany <calemany@student.42.fr>
2016-04-21 10:05:08 +0200

    Refactor

--- src/three/ObjectConvert.js
+++ src/three/ObjectConvert.js
@@ -3,16 +3,21 @@ var AMTHREE = AMTHREE || {};
 
 (function() {
 
+  function IsDef(val) {
+    return typeof val != 'undefined' && val != null;
+  }
 
   function ObjectConvert(object) {
     object.traverse(function(child) {
 
-      if (child.material && child.material.map) {
-        if (child.material.map.image) {
-          var image = new AMTHREE.Image(undefined, child.material.map.image.src);
-          var texture = new AMTHREE.ImageTexture(image);
-          child.material.map = texture;
-          child.material.needsUpdate = true;
+      if (IsDef(child.material) && IsDef(child.material.map)) {
+        return function() {
+          if (IsDef(child.material.map.image)) {
+            var image = new AMTHREE.Image(undefined, child.material.map.image.src);
+            var texture = new AMTHREE.ImageTexture(image);
+            child.material.map = texture;
+            child.material.needsUpdate = true;
+          }
         }
       }
 
