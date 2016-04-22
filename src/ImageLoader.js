89f3834d60f54018fd5bb7a994e1d3d263998d4a
calemany <calemany@student.42.fr>
2016-04-22 17:16:07 +0200

    Replace ImageLoader by two functions

--- src/ImageLoader.js
+++ src/ImageLoader.js
@@ -1,41 +1,64 @@
 var AM = AM || {};
 
-
-AM.ImageLoader = function() {
-
-  var _canvas = document.createElement('canvas');
-  var _ctx = _canvas.getContext('2d');
+(function() {
 
 
-  this.GetImageData = function(url, on_load, square) {
-    var img = new Image();
-    
-    img.onload = function(img, on_load, square) {
-      return function() {
+  /**
+  * Loads an image.
+  * @param {string} url
+  * @param {Image} [image] - An image element in which to load the image.
+  * @returns {Promise<Image, string>} A promise that resolves when the image is loaded.
+  */
+  AM.LoadImage = function(url, image) {
+    return new Promise(function(resolve, reject) {
+      image = image || new Image();
 
-        if (square) {
-          var size = Math.max(img.width, img.height);
-          var x = (size - img.width)  / 2;
-          var y = (size - img.height) / 2;
-
-          _canvas.width = size;
-          _canvas.height = size;
-
-          _ctx.drawImage(img, x, y);
-        }
-        else {
-          _canvas.width = img.width;
-          _canvas.height = img.height;
-
-          _ctx.drawImage(img, 0, 0, _canvas.width, _canvas.height);
-        }
-
-        var image_data = _ctx.getImageData(0, 0, _canvas.width, _canvas.height);
-
-        on_load(image_data);
+      image.src = null;
+      image.onload = function() {
+        resolve(image);
       };
-    }(img, on_load, square);
-    img.src = url;
+      image.onerror = function(e) {
+        reject(e);
+      };
+      image.src = url;
+    });
   };
 
-};
+  /**
+  * Returns the ImageData of an image element.
+  * @param {Image} img
+  * @param {bool} square - if true, the image will be copied inside a square matrix.
+  * @returns {ImageData}
+  */
+  AM.ImageToImageData = (function() {
+
+    var _canvas = document.createElement('canvas');
+    var _ctx = _canvas.getContext('2d');
+
+    return function(img, square) {
+      if (square) {
+        var size = Math.max(img.width, img.height);
+        var x = (size - img.width)  / 2;
+        var y = (size - img.height) / 2;
+
+        _canvas.width = size;
+        _canvas.height = size;
+
+        _ctx.clearRect(0, 0, _canvas.width, _canvas.height);
+        _ctx.drawImage(img, x, y);
+      }
+      else {
+        _canvas.width = img.width;
+        _canvas.height = img.height;
+
+        _ctx.clearRect(0, 0, _canvas.width, _canvas.height);
+        _ctx.drawImage(img, 0, 0, _canvas.width, _canvas.height);
+      }
+
+      return _ctx.getImageData(0, 0, _canvas.width, _canvas.height);
+    };
+
+  })();
+
+
+})();
