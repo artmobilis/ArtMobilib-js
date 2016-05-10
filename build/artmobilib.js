96007acac2b3bd8da9dc915c5ac01eeb37c1e451
calemany <calemany@student.42.fr>
2016-05-10 12:10:07 +0200

    Update build

--- build/artmobilib.js
+++ build/artmobilib.js
@@ -1248,6 +1248,53 @@ var AM = AM || {};
 
 })();
 
+var AM = AM || {};
+
+(function() {
+
+  var INTERRUPT_MSG = 'UniqueLoader interrupted';
+
+  function UniqueLoader(fun) {
+    this._fctn = fun;
+    this._interrupted = false;
+    this._launched = false;
+  }
+
+  UniqueLoader.prototype.Load = function() {
+    var scope = this;
+
+    if (this._launched)
+      throw new Error('UniqueLoader.Load is called more than once');
+    if (this._interrupted)
+      return Promise.reject(INTERRUPT_MSG);
+
+    this._launched = true;
+
+    return new Promise(function(resolve, reject) {
+      if (!scope._interrupted) {
+        Promise.resolve(scope._fctn()).then(function() {
+          if (scope._interrupted)
+            reject(INTERRUPT_MSG);
+          else
+            resolve(arguments);
+        }, function(e) {
+          reject(scope._interrupted ? INTERRUPT_MSG : e);
+        });
+      }
+      else
+        reject(INTERRUPT_MSG);
+    });
+  };
+
+  UniqueLoader.prototype.Interrupt = function() {
+    if (this._launched)
+      this._interrupted = true;
+  };
+
+
+  AM.UniqueLoader = UniqueLoader;
+
+})();
 /**************
 
 DeviceLockScreenOrientation
@@ -10916,7 +10963,7 @@ AM.MarkerTracker = function() {
     match_min : 8
   };
   
-  var _debug =true; 
+  var _debug =false; 
   
   var _profiler = new AM.Profiler();
   _profiler.add('filter');
@@ -11815,7 +11862,7 @@ AM.Training = function() {
     var corners_num = 0;
     if (_use_fixed_angle)
       corners_num = AM.DetectKeypointsYape06(level_img, corners, _params.training_corners_max,
-        _params.laplacian_threshold, _params.eigen_threshold, undefined, -Math.PI / 2);
+        _params.laplacian_threshold, _params.eigen_threshold, undefined, 0);
     else
       corners_num = AM.DetectKeypointsYape06(level_img, corners, _params.training_corners_max,
         _params.laplacian_threshold, _params.eigen_threshold);
